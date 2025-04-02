b9791b2b10d180cc920d21f0037c366e46701982
Fabio Riccato <fabio.riccato2007@gmail.com>
2025-04-02 23:14:27 +0200

    Aggiunta di nuovi metodi per la ricerca dei dati

--- Server/ServerRifiutiVeneto/src/GestoreDati.java
+++ Server/ServerRifiutiVeneto/src/GestoreDati.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class GestoreDati {
@@ -35,24 +36,6 @@ public class GestoreDati {
         return "ERROR: riga non trovata";
     }
 
-    public String rifiutiProdotti(){
-        String pericolosi = "pericolosi";
-        String specialiNp = "speciali np";
-        double iPericolosi = 0;
-        int iSpeciali = 0;
-        for(Dato dato: dati){
-            if(dato.getTipoRifiuto().equalsIgnoreCase(pericolosi)){
-                iPericolosi += dato.getQuantità();
-            }else{
-               iSpeciali += dato.getQuantità();
-            }
-        }
-        if(iSpeciali > iPericolosi){
-            return "La regione veneto produce più rifiuti " +specialiNp;
-        }
-        return "La regione veneto produce puù rifiuti "+pericolosi;
-    }
-
     public String provinciaRifiutiAnno(int anno){
         double quantità = 0;
         String provincia = "";
@@ -67,6 +50,108 @@ public class GestoreDati {
         return "La provincia con più rifiuti prodotti nell'anno: "+anno+" è: "+provincia;
     }
 
+    public String tipoRifiutiProdotti(){
+        String pericolosi = "pericolosi";
+        String specialiNp = "speciali np";
+        double iPericolosi = 0;
+        double iSpeciali = 0;
+        for(Dato dato: dati){
+            if(dato.getTipoRifiuto().equalsIgnoreCase(pericolosi)){
+                iPericolosi += dato.getQuantità();
+            }else{
+               iSpeciali += dato.getQuantità();
+            }
+        }
+        if(iSpeciali > iPericolosi){
+            return "La regione veneto produce più rifiuti di tipo: " +specialiNp;
+        }
+        return "La regione veneto produce più rifiuti di tipo: "+pericolosi;
+    }
+
+    public String tipoRifiutiProdottiAnno(int anno){
+        String pericolosi = "pericolosi";
+        String specialiNp = "speciali np";
+        double iPericolosi = 0;
+        double iSpeciali = 0;
+        for(Dato dato: dati){
+            if(dato.getAnno() == anno){
+                if(dato.getTipoRifiuto().equalsIgnoreCase(pericolosi)){
+                    iPericolosi += dato.getQuantità();
+                }else{
+                    iSpeciali += dato.getQuantità();
+                }
+            }
+        }
+        if(iSpeciali > iPericolosi){
+            return "Nell'anno: "+anno+" sono stati prodotti più rifiuti di tipo: " +specialiNp;
+        }
+        return "Nell'anno: "+anno+" sono stati prodotti più rifiuti di tipo: "+pericolosi;
+    }
+
+    public String tipoRifiutiProdottiProvincia (String provincia){
+        String pericolosi = "pericolosi";
+        String specialiNp = "speciali np";
+        double iPericolosi = 0;
+        double iSpeciali = 0;
+        for(Dato dato: dati){
+            if(dato.getProvincia().equalsIgnoreCase(provincia)){
+                if(dato.getTipoRifiuto().equalsIgnoreCase(pericolosi)){
+                    iPericolosi += dato.getQuantità();
+                }else{
+                    iSpeciali += dato.getQuantità();
+                }
+            }
+        }
+        if(iSpeciali > iPericolosi){
+            return "La provincia: "+provincia+" ha prodotto più rifiuti di tipo: " +specialiNp;
+        }else if(iPericolosi == iSpeciali){
+            return "Provincia non trovata";
+
+        }
+        return "La provincia: "+provincia+" ha prodotto più rifiuti di tipo: "+pericolosi;
+    }
+
+    public String annoRifiuti(){
+        double nRifiuti = 0;
+        double nRifiutiMax = 0;
+        int annoMagg = 0;
+        int anno = 0;
+        for(Dato dato: dati){
+            if(dato.getAnno() != anno){
+                nRifiuti = 0;
+                anno = dato.getAnno();
+
+            }
+            nRifiuti += dato.getQuantità();
+            if(nRifiuti > nRifiutiMax){
+                nRifiutiMax = nRifiuti;
+                annoMagg = anno;
+            }
+        }
+        return "La regione Veneto ha prodotto più rifiuti nel: "+annoMagg;
+    }
+
+    public String provinciaRifiuti (){
+        ArrayList <Dato> tmp = dati;
+        Collections.sort(tmp);
+        double nRifiuti = 0;
+        double nRifiutiMax = 0;
+        String provincia = "";
+        String provinciaMax = "";
+        for(Dato dato: dati){
+            if(!dato.getProvincia().equalsIgnoreCase(provincia)){
+                nRifiuti = 0;
+                provincia = dato.getProvincia();
+
+            }
+            nRifiuti += dato.getQuantità();
+            if(nRifiuti > nRifiutiMax){
+                nRifiutiMax = nRifiuti;
+                provinciaMax = provincia;
+            }
+        }
+        return "La provincia che ha prodotto più rifiuti è : "+provinciaMax;
+    }
 
     @Override
     public String toString() {
