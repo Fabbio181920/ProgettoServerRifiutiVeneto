7f40b947ad60fdcab635dbf56c8adc1a20ace677
Riccato Fabio <fabio.riccato@cz.lan>
2025-05-06 13:40:49 +0200

    Modifiche finali

--- Client/ClientRifiutiVeneto/src/Main.java
+++ Client/ClientRifiutiVeneto/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 import javax.swing.*;
 
@@ -5,7 +6,7 @@ import javax.swing.*;
  * Creazione della schermata iniziale con il menù
  */
 public class Main {
-    final static String nomeServer = "localhost";
+    public static String nomeServer = "localhost";
     final static int portaServer = 1050;
     public static String [] titoli = {"Spegni client e server", "Riga del file", "Provincia che ha prodotto più rifiuti in un anno", "Quantità di rifiuti prodotti per tipo dalla regione", "Quantità di rifiuti prodotti per tipo in un anno", "Quantità di rifiuti prodotti per tipo da una provincia", "Anno in cui sono stati prodotti più rifiuti", "Provincia che ha prodotto più rifiuti" };
     public static String[] opzioni = {"Spegni client e server", "Riga del file",
@@ -16,18 +17,44 @@ public class Main {
             "<html><div style='text-align: center;'>Anno in cui sono stati <br>prodotti più rifiuti</div></html>",
             "<html><div style='text-align: center;'>Provincia che ha <br>prodotto più rifiuti</div></html>"};
 
-    public static JFrame menù;
+    public static JFrame frame;
+    public static JPanel content = new JPanel();
+    public static JPanel menù = new JPanel();
     public static void main(String[] args) {
         System.out.println("Connessione al server in corso...");
-        menù = new JFrame("Menù");
-        menù.setSize(1000, 400);
-        menù.setIconImage(new ImageIcon("Regione_Veneto.png").getImage());
-        menù.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE);
+        frame = new JFrame("Menù");
+        frame.setSize(800, 300);
+        frame.setIconImage(new ImageIcon("Regione_Veneto.png").getImage());
+        frame.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.add(menù);
         menù.setLayout(new BorderLayout());
         JLabel descrizione = new JLabel("Selezionare l'opzione", SwingConstants.CENTER);
         descrizione.setFont(new Font("Arial", Font.BOLD, 18));
         descrizione.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         menù.add(descrizione, BorderLayout.NORTH);
+        JLabel modificaIp =new JLabel("Inserisci un nuovo indirizzo IP (opzionale): ");
+        modificaIp.setFont(new Font("Arial", Font.BOLD, 18));
+        JTextField insIp = new JTextField(10);
+        insIp.setFont(new Font("Arial", Font.BOLD, 14));
+        insIp.setForeground(Color.BLACK);
+        insIp.setMargin(new Insets(5, 10, 8, 100));
+        JButton button = new JButton("Invia");
+        button.addActionListener(e -> {
+            String ip = insIp.getText();
+            if(verIp(ip)){
+                nomeServer = ip;
+            }
+            System.out.println(nomeServer
+            );
+        });
+        modificaIp.setVisible(true);
+        insIp.setVisible(true);
+        button.setVisible(true);
+        content.add(modificaIp, BorderLayout.SOUTH);
+        content.add(insIp,BorderLayout.SOUTH);
+        content.add(button);
+        frame.add(content);
 
 
         JPanel p = new JPanel();
@@ -44,7 +71,21 @@ public class Main {
         }
         menù.add(p, BorderLayout.CENTER);
         menù.setVisible(true);
+        frame.setVisible(true);
 
+    }
+
+    public static boolean verIp(String ip){
+        int count = 0;
+        for(int i = 0; i < ip.length(); i++){
+            if(ip.substring(i, i+1).equalsIgnoreCase(".")){
+                count ++;
+            }
+        }
+        if(count == 3){
+            return true;
+        }
+        return false;
     }
 
 }
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ ISTRUZIONI PER L'ESECUZIONE:
 1. Clonare il prgetto in locale tramite il comando "git clone /link repository remota git/".
 2. Aprire la cartella contenente il progettto del Server tramite IntelliJ e mandare in esecuzione il main (Path: \ProgettoServerRifiutiVeneto\Server).
 3. Aprire la cartella contenente il progetto del Client tramite IntelliJ e mandare i esecuzione il main (Path: \ProgettoServerRifiutiVeneto\Client).
+4. Inserire un nuovo indirizzo IP di un dispositivo nella rete locale se necessario (se non inserito il client cercherà una connessione con localHost)
 4. Si aprirà una finestra e basterà interagire con essa per ottenere informazioni dal server.
 
 Per terminare correttamente i processi di client e server è sufficiente slezionare l'opzione "Spegni client e server".
